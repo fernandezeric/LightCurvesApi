9ace7d365b99ebc3648c2ae39391b2836f23d295
fernandezeric <ericfernandezgonzalez@gmail.com>
2020-08-19 12:42:54 -0400

    integration test

--- LightCurvesApi/tests/test_ps1rute.py
+++ LightCurvesApi/tests/test_ps1rute.py
@@ -4,3 +4,77 @@ sys.path.append("..")
 
 from app import app
 from flask import json
+
+def test_radio_degree():
+    client = app.test_client()
+    url = '/radio-degree'
+
+    headers = {
+        'content-type': 'application/json',
+    }
+
+    data = {
+        'ra':'139.33444972',
+        'dec':'68.6350604',
+        'radius':'0.0002777',
+        'format':'csv',
+        'catalog':'ps1'
+        }
+
+    response = client.post(url, data=(data)) #data=json.dumps(data)
+    assert response.status_code == 200
+
+def test_radio_degree_nearest():
+    client = app.test_client()
+    url = '/radio-degree-nearest'
+
+    headers = {
+        'content-type': 'application/json',
+    }
+
+    data = {
+        'ra':'139.33444972',
+        'dec':'68.6350604',
+        'radius':'0.0002777',
+        'format':'csv',
+        'catalog':'ps1'
+        }
+
+    response = client.post(url, data=(data)) #data=json.dumps(data)
+    assert response.status_code == 200
+
+def test_radio_hms():
+    client = app.test_client()
+    url = '/radio-hms'
+
+    headers = {
+        'content-type': 'application/json',
+    }
+
+    data =  {
+        'hms':'9h17m20.26793280000689s +4h34m32.414496000003936s',
+        'radius':'0.0002777',
+        'format':'csv',
+        'catalog':'ps1'
+        }
+
+    response = client.post(url, data=(data)) #data=json.dumps(data)
+    assert response.status_code == 200
+
+def test_radio_hms_nearest():
+    client = app.test_client()
+    url = '/radio-hms-nearest'
+
+    headers = {
+        'content-type': 'application/json',
+    }
+
+    data =  {
+        'hms':'9h17m20.26793280000689s +4h34m32.414496000003936s',
+        'radius':'0.0002777',
+        'format':'csv',
+        'catalog':'ps1'
+        }
+
+    response = client.post(url, data=(data)) #data=json.dumps(data)
+    assert response.status_code == 200
--- LightCurvesApi/tests/test_ztfrute.py
+++ LightCurvesApi/tests/test_ztfrute.py
@@ -6,44 +6,6 @@ sys.path.append("..")
 from app import app
 from flask import json
 
-
-# def test_radio_degree():
-#     params = {'ra':'139.33444972','dec':'68.6350604','radius':'0.0002777','format':'csv','catalog':'ztf'}
-#     response = app.test_client().post(
-#         '/radio-degree',
-#         data = params
-#         )
-#     json_data = response.get_json()
-#     assert response.status_code == 200
-
-# def test_radio_degree_nearest():
-#     params = {'ra':'139.33444972','dec':'68.6350604','radius':'0.0002777','format':'csv','catalog':'ztf'}
-#     response = app.test_client().post(
-#         '/radio-degree-nearest',
-#         data = params
-#         )
-#     json_data = response.get_json()
-#     assert response.status_code == 200
-
-# def test_radio_hms():
-#     params = {'hms':'9h17m20.26793280000689s +4h34m32.414496000003936s','radius':'0.0002777','format':'csv','catalog':'ztf'}
-#     response = app.test_client().post(
-#         '/radio-hms-nearest',
-#         data = params
-#         )
-#     json_data = response.get_json()
-#     assert response.status_code == 200
-
-# def test_radio_hms_nearest():
-#     params = {'hms':'9h17m20.26793280000689s +4h34m32.414496000003936s','radius':'0.0002777','format':'csv','catalog':'ztf'}
-#     response = app.test_client().post(
-#         '/radio-hms-nearest',
-#         data = params
-#         )
-#     json_data = response.get_json()
-#     assert response.status_code == 200
-
-
 def test_radio_degree():
     client = app.test_client()
     url = '/radio-degree'
@@ -60,5 +22,60 @@ def test_radio_degree():
         'catalog':'ztf'
         }
 
-    response = client.post(url, data=json.dumps(data),headers=headers)
+    response = client.post(url, data=(data)) #data=json.dumps(data)
+    assert response.status_code == 200
+
+def test_radio_degree_nearest():
+    client = app.test_client()
+    url = '/radio-degree-nearest'
+
+    headers = {
+        'content-type': 'application/json',
+    }
+
+    data = {
+        'ra':'139.33444972',
+        'dec':'68.6350604',
+        'radius':'0.0002777',
+        'format':'csv',
+        'catalog':'ztf'
+        }
+
+    response = client.post(url, data=(data)) #data=json.dumps(data)
+    assert response.status_code == 200
+
+def test_radio_hms():
+    client = app.test_client()
+    url = '/radio-hms'
+
+    headers = {
+        'content-type': 'application/json',
+    }
+
+    data =  {
+        'hms':'9h17m20.26793280000689s +4h34m32.414496000003936s',
+        'radius':'0.0002777',
+        'format':'csv',
+        'catalog':'ztf'
+        }
+
+    response = client.post(url, data=(data)) #data=json.dumps(data)
+    assert response.status_code == 200
+
+def test_radio_hms_nearest():
+    client = app.test_client()
+    url = '/radio-hms-nearest'
+
+    headers = {
+        'content-type': 'application/json',
+    }
+
+    data =  {
+        'hms':'9h17m20.26793280000689s +4h34m32.414496000003936s',
+        'radius':'0.0002777',
+        'format':'csv',
+        'catalog':'ztf'
+        }
+
+    response = client.post(url, data=(data)) #data=json.dumps(data)
     assert response.status_code == 200
